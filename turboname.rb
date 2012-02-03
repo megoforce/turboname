225c7f1b0f1ad63a821ab626a98fdaf4b292890b
phaser <phaser@tubatuba.net>
2012-02-03 19:53:52 -0300

    ahora con nombres en espanol

--- turboname.rb
+++ turboname.rb
@@ -10,7 +10,7 @@ def domainsearch(name)
 end
 def newname
   vowels=["a","e","i","o","u"]
-  line = File.readlines("dictionary.txt")[rand(349899)-1].gsub("\n","")
+  line = File.readlines("dictionary.txt")[rand(435000)-1].gsub("\n","")
   2.times do
     line=line.gsub(vowels.choice,vowels.choice) if rand(1)==1
   end
