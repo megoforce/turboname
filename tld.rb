d2ea6f4147b134029b5879577c23eb8f34b4cb25
Nicolas Justiniano <n.justiniano@gmail.com>
2012-02-03 18:21:41 -0300

    tld's array

---
+++ tld.rb
@@ -0,0 +1,107 @@
+require 'uri'
+
+puts posibilidades(dominio)
+
+def posibilities(dominio)
+  if (resultado =~ URI::regexp).nil?
+      puts 'wena'
+  else
+    puts 'shit'
+  end
+  return resultado
+end
+
+countrytld => [
+        'ac',
+        'ad','ae','af','ag',
+        'ai','al','am','an',
+        'ao','aq','ar','as',
+        'at','au','aw','ax',
+        'az','ba','bb','bd',
+        'be','bf','bg','bh',
+        'bi','bj','bm','bn',
+        'bo','br','bs','bt',
+        'bu','bv','bw','by',
+        'bz','ca','cc','cd',
+        'cf','cg','ch','ci',
+        'ck','cl','cm','cn',
+        'co','cr','cs','cu',
+        'cv','cx','cy','cz',
+        'de','dj','dk','dm',
+        'do','dz','ec','ee',
+        'eg','eh','er','es',
+        'et','eu','fi','fj',
+        'fk','fm','fo','fr',
+        'ga','gb','gd','ge',
+        'gf','gg','gh','gi',
+        'gl','gm','gn','gp',
+        'gq','gr','gs','gt',
+        'gu','gw','gy','hk',
+        'hm','hn','hr','ht',
+        'hu','id','ie','il',
+        'im','in','io','iq',
+        'ir','is','it','je',
+        'jm','jo','jp','ke',
+        'kg','kh','ki','km',
+        'kn','kp','kr','kw',
+        'ky','kz','la','lb',
+        'lc','li','lk','lr',
+        'ls','lt','lu','lv',
+        'ly','ma','mc','md',
+        'me','mg','mh','mk',
+        'ml','mm','mn','mo',
+        'mp','mq','mr','ms',
+        'mt','mu','mv','mw',
+        'mx','my','mz','na',
+        'nc','ne','nf','ng',
+        'ni','nl','no','np',
+        'nr','nu','nz','om',
+        'pa','pe','pf','pg',
+        'ph','pk','pl','pm',
+        'pn','pr','ps','pt',
+        'pw','py','qa','re',
+        'ro','rs','ru','rw',
+        'sa','sb','sc','sd',
+        'se','sg','sh','si',
+        'sj','sk','sl','sm',
+        'sn','so','sr','st',
+        'su','sv','sy','sz',
+        'tc','td','tf','tg',
+        'th','tj','tk','tl',
+        'tm','tn','to','tp',
+        'tr','tt','tv','tw',
+        'tz','ua','ug','uk',
+        'us','uy','uz','va',
+        'vc','ve','vg','vi',
+        'vn','vu','wf','ws',
+        'ye','yt','yu','za',
+        'zm','zw',
+]
+    
+generictld => [
+            'aero',
+            'biz',
+            'cat',
+            'com',
+            'coop',
+            'edu',
+            'gov',
+            'info',
+            'int',
+            'jobs',
+            'mil',
+            'mobi',
+            'museum',
+            'name',
+            'net',
+            'org',
+            'pro',
+            'travel',
+            'asia',
+            'post',
+            'tel',
+            'geo',
+]
+
+   
+ 
