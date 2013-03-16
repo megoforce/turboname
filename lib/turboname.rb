661b94fa22c61e7e1077f083f85145fda1cb44fb
José Tomás Albornoz <jojo@eljojo.net>
2013-03-16 15:40:23 +0100

    we haz gem

--- lib/turboname.rb
+++ lib/turboname.rb
@@ -2,6 +2,11 @@ require "turboname/version"
 require "whois"
 
 module Turboname
+  
+  def self.path
+    File.dirname(__FILE__)
+  end
+  
   class Domain
     CountryTLDs = %w{ac ad ae af ag ai al am an ao aq ar as at au aw ax az ba bb bd be bf bg bh bi bj bm bn bo br bs bt bu bv bw by bz ca cc cd cf cg ch ci ck cl cm cn co cr cs cu cv cx cy cz de dj dk dm do dz ec ee eg eh er es et eu fi fj fk fm fo fr ga gb gd ge gf gg gh gi gl gm gn gp gq gr gs gt gu gw gy hk hm hn hr ht hu id ie il im in io iq ir is it je jm jo jp ke kg kh ki km kn kp kr kw ky kz la lb lc li lk lr ls lt lu lv ly ma mc md me mg mh mk ml mm mn mo mp mq mr ms mt mu mv mw mx my mz na nc ne nf ng ni nl no np nr nu nz om pa pe pf pg ph pk pl pm pn pr ps pt pw py qa re ro rs ru rw sa sb sc sd se sg sh si sj sk sl sm sn so sr st su sv sy sz tc td tf tg tj tk tl tm tn to tp tr tt tv tw tz ua ug uk us uy uz va vc ve vg vi vn vu wf ws ye yt yu za zm zw}
   
@@ -56,7 +61,7 @@ module Turboname
   class Random < Dictionary
     def get
       vowels=["a","e","i","o","u"]
-      line = File.readlines("dictionary.txt")[rand(435000)-1].gsub("\n","")
+      line = File.readlines(File.join(Turboname::path, 'dictionary.txt'))[rand(435000)-1].gsub("\n","")
       rand(20).times do
         line=line.gsub(vowels.sample,vowels.sample)
       end
